5045ad6197edde721ab5c3c30b5d039b0de82219
tahayarkhan <tahayarkhan03@gmail.com>
2026-06-19 18:08:15 -0400

    UI changes on navbar

--- frontend/src/components/navigation.jsx
+++ frontend/src/components/navigation.jsx
@@ -6,17 +6,17 @@ const Navigation = () => {
   const tabs = ["+", "Items", "Outfits", "Wardrobe"];
 
   return (
-    <nav className="self-center bg-black my-5 px-6 py-2 rounded-bl-full w-3/4 h-full">
+    <nav className="self-center bg-black my-5 px-6 py-2 rounded-full w-3/4 h-full">
       <div className="flex justify-center gap-4">
         {tabs.map((tab) => (
           <button
             key={tab}
             onClick={() => setActiveTab(tab)}
-            className={`rounded-md px-4 py-2 text-sm font-normal transition-colors
+            className={`rounded-xl px-4 py-1 text-sm font-normal transition-colors
               ${
                 activeTab === tab
                   ? "bg-white text-black"
-                  : "text-gray-300 hover:bg-white/10 hover:text-white"
+                  : "text-gray-300 hover:bg-white/20 hover:text-white"
               }`}
           >
             {tab}
