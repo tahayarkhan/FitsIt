0fa424be6912de9b4928d7172c628d44dfb9de16
tahayarkhan <tahayarkhan03@gmail.com>
2026-06-19 18:13:14 -0400

    UI changes

--- frontend/src/components/navigation.jsx
+++ frontend/src/components/navigation.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import Upload from "./upload";
+import Items from "./items";
+import Recommendations from "./recommendations";
 
 const Navigation = () => {
   const [activeTab, setActiveTab] = useState("+");
@@ -6,13 +9,14 @@ const Navigation = () => {
   const tabs = ["+", "Items", "Outfits", "Wardrobe"];
 
   return (
-    <nav className="self-center bg-black my-5 px-6 py-2 rounded-full w-3/4 h-full">
+    <>
+    <nav className="self-center bg-black my-10 px-6 py-2 rounded-full w-3/4 h-full">
       <div className="flex justify-center gap-4">
         {tabs.map((tab) => (
           <button
             key={tab}
             onClick={() => setActiveTab(tab)}
-            className={`rounded-xl px-4 py-1 text-sm font-normal transition-colors
+            className={`rounded-xl px-4 py-1.5 text-sm font-normal transition-colors
               ${
                 activeTab === tab
                   ? "bg-white text-black"
@@ -24,6 +28,15 @@ const Navigation = () => {
         ))}
       </div>
     </nav>
+    
+    <div className="mt-6">
+        {activeTab === "Add Item" && <Upload />}
+        {activeTab === "Items" && <Items />}
+        {activeTab === "Outfits" && <Recommendations />}
+        {/* {activeTab === "Wardrobe" && <Wardrobe />} */}
+    </div>
+    </>
+    
   );
 };
 
