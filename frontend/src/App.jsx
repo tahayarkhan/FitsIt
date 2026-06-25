aac2fa19f062541f55bbc92a97f95a36e8916f8a
tahayarkhan <tahayarkhan03@gmail.com>
2026-06-25 14:25:21 -0400

    UI changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import Navigation from './components/navigation'
+import { motion } from "framer-motion"
+
 
 function App() {
 
   return (
     <div className="flex min-h-screen flex-col items-center bg-gray-50">
       <header className="mt-10 w-full text-center">
-        <h1 className="text-6xl font-bold tracking-tight text-gray-900 sm:text-7xl">FitsIt</h1>
+        <motion.h1
+          whileHover={{scale:1.5}}
+          className="text-6xl font-bold tracking-tight text-gray-900 sm:text-7xl"
+        >
+          FitsIt
+        </motion.h1>
       </header>
       <Navigation/>
     </div>
